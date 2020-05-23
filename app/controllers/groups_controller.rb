390fb68b97bcb795c235098a51bc0ef6209382c4
yukiochiai <ochiai.y41@gmail.com>
2020-05-23 16:01:15 +0900

    送信機能完了

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,7 +10,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
+      redirect_to group_path, notice: 'グループを作成しました'
     else 
       render :new
   end
@@ -23,7 +23,7 @@ end
 def update
   @group = Group.find(params[:id])
   if @group.update(group_params)
-    redirect_to root_path, notice: 'グループを更新しました'
+    redirect_to group_messages_path(@group.id), notice: 'グループを更新しました'
   else
     render :edit
   end
